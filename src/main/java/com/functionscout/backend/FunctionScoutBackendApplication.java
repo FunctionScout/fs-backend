aedb971877ca744d6c42b17f204b35b85e7b504d
Subramanya <keshavas@oregonstate.edu>
2024-02-04 03:26:33 -0800

    Setup project skeleton

---
+++ src/main/java/com/functionscout/backend/FunctionScoutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.functionscout.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionScoutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionScoutBackendApplication.class, args);
+	}
+
+}
