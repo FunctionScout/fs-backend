5f38eacb089baaae6a7ba375e9b10a28bf391665
Subramanya <keshavas@oregonstate.edu>
2024-05-09 15:13:55 -0700

    feat: saving classes with their fully-qualified name

--- src/main/java/com/functionscout/backend/parser/ClassParser.java
+++ src/main/java/com/functionscout/backend/parser/ClassParser.java
@@ -1,8 +1,10 @@
 package com.functionscout.backend.parser;
 
+import com.functionscout.backend.dto.ClassDTO;
 import com.functionscout.backend.dto.FunctionDTO;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
@@ -12,17 +14,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ClassParser {
-    public List<FunctionDTO> extractFunctions(final Path filePath) {
+    public ClassDTO extractFunctions(final String className, final Path filePath) {
         try {
             final CompilationUnit cu = StaticJavaParser.parse(Files.newInputStream(filePath));
+            final Optional<PackageDeclaration> packageDeclaration = cu.getPackageDeclaration();
+            String packageName = "";
+
+            if (packageDeclaration.isPresent()) {
+                packageName = packageDeclaration.get().getNameAsString();
+            }
+
             final VoidVisitor<List<FunctionDTO>> methodCollector = new MethodNameCollector();
             final List<FunctionDTO> functionDTOS = new ArrayList<>();
             methodCollector.visit(cu, functionDTOS);
 
-            return functionDTOS;
+            return new ClassDTO(packageName + "." + className, functionDTOS);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
