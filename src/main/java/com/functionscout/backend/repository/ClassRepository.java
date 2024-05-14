182f37f914a0c5afefea18c237d3954497adf931
Subramanya <keshavas@oregonstate.edu>
2024-05-14 02:36:48 -0700

    feat: isolating imports and exploring their usages

--- src/main/java/com/functionscout/backend/repository/ClassRepository.java
+++ src/main/java/com/functionscout/backend/repository/ClassRepository.java
@@ -2,9 +2,15 @@ package com.functionscout.backend.repository;
 
 import com.functionscout.backend.model.Class;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 
 @Repository
 public interface ClassRepository extends JpaRepository<Class, Integer> {
+
+    @Query("select c.name from Class c")
+    Set<String> findAllNames();
 }
