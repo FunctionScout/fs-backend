fd80948731109a6f399c88ea65f79e0dbf769e91
Subramanya <keshavas@oregonstate.edu>
2024-05-19 12:59:49 -0700

    supporting two way dependency scanning

--- src/main/java/com/functionscout/backend/repository/DependencyRepository.java
+++ src/main/java/com/functionscout/backend/repository/DependencyRepository.java
@@ -4,6 +4,10 @@ import com.functionscout.backend.model.Dependency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DependencyRepository extends JpaRepository<Dependency, Integer> {
+
+    Optional<Dependency> findByName(String name);
 }
